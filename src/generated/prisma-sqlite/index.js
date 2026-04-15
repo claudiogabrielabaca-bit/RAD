85cc77332b74b29d87e70761809af8e1a9ebad12
Gabriel Abaca <claudiogabrielabaca@gmail.com>
2026-04-14 22:12:57 -0300

    Add SQLite to Postgres migration tooling

--- src/generated/prisma-sqlite/index.js
+++ src/generated/prisma-sqlite/index.js
@@ -343,7 +343,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
