b92cce4921a0f16ebd2191cba184b2e328bd77fa
Gabriel Abaca <claudiogabrielabaca@gmail.com>
2026-04-15 21:58:55 -0300

    Remove surprise bootstrap from server page render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import HomePageClient from "./home-page-client";
 import { buildDayBundle } from "@/app/lib/day-bundle";
-import { getCurrentUser } from "@/app/lib/current-user";
-import { getNextSurpriseDay } from "@/app/lib/surprise-deck";
 
 function isValidDayString(value?: string | null): value is string {
   return !!value && /^\d{4}-\d{2}-\d{2}$/.test(value);
@@ -32,15 +30,6 @@ export default async function Page({
   try {
     if (isValidDayString(requestedDay)) {
       initialBundle = await buildDayBundle(requestedDay);
-    } else {
-      const user = await getCurrentUser();
-      const surprise = await getNextSurpriseDay({
-        userId: user?.id ?? null,
-      });
-
-      if (surprise?.day) {
-        initialBundle = await buildDayBundle(surprise.day);
-      }
     }
   } catch (error) {
     console.error("page initial bundle error:", error);
